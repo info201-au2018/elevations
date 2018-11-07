a4492f2f30b4f44c1e11b9f38141176dc7251e74
Ott Toomet <otoomet@uw.edu>
2018-11-07 12:13:08 -0800

    good colors

--- plot.R
+++ plot.R
@@ -1,9 +1,13 @@
 library(ggplot2)
+library(dplyr)
 
-data_table <- read.delim("uw-elevations.csv.bz2")
+data_table <- read.delim("data/uw-elevations.csv.bz2") %>%
+   mutate(elevation = ifelse(elevation < 5.5, NA, elevation))
 
 ggplot(data_table, aes(lon, lat, fill=elevation)) +
    geom_tile() +
    coord_quickmap() +
-   xlab("Longitude") + ylab("Latitude")
+   xlab("Longitude") + ylab("Latitude") +
+   scale_fill_gradientn(colors=terrain.colors(10),
+                        na.value="steelblue3")
 
