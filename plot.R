74afaa45ef6a410287ae960598b4457e02855be4
Ott Toomet <otoomet@uw.edu>
2018-11-07 11:58:05 -0800

    use geom_tile

--- plot.R
+++ plot.R
@@ -2,6 +2,6 @@ library(ggplot2)
 
 data <- read.delim("data/uw-elevations.csv.bz2")
 
-ggplot(data, aes(lon, lat, col=elevation)) +
-   geom_point() +
+ggplot(data, aes(lon, lat, fill=elevation)) +
+   geom_tile() +
    coord_quickmap()
